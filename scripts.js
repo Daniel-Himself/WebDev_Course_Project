1d48b2cd19c125347af99a52f0abaa35bff16b72
Daniel-Himself <danielsharon97@gmail.com>
2022-04-08 15:14:17 +0300

    Commented out adding entries to the table because it isn't needed for the first assignment

--- scripts.js
+++ scripts.js
@@ -1,17 +1,3 @@
-// Code to add entried to the recipy table. Will be relevant later on
-
-// $(document).ready(function() {
-//     $("#add_user").click(function(){
-//         let first_name = $("#first_name").val();
-//         let last_name = $("#last_name").val();
-//         let phone_number = $("#phone").val();
-//         addrow(first_name, last_name, phone_number);
-//         function addrow(first_name, last_name, phone_number){
-//             let row = "<tr><td>" + first_name + "</td><td>" + last_name + "</td><td>" + phone_number + "</td></tr>";
-//             $("#recipes").append(row);
-//         }
-//     })
-//
-//
-//
-// });
+$(document).ready(function() {
+    $('#recipes').DataTable();
+} );
