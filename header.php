549cc684221bdcf729fa43797561f1f459593f29
Daniel-Himself <danielsharon97@gmail.com>
2022-05-07 17:56:50 +0300

    Created a unified header file

---
+++ header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+    <!--    local css stylesheet-->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+
+    <!-- local JS file -->
+    <script src="scripts.js"></script>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+
+    <title>Recipes</title>
+</head>
+<body>
+<header>
+    <nav class="navbar  navbar-expand-sm navbar-light navbar-laravel fixed-top bg-light text-dark ">
+        <div class="container">
+            <a class="navbar-brand" href="#">Ali and Daniel's Recipe Book</a>
+
+            <!-- TODO (broken) Code for hamburger menu that auto hides the navigation bar items on small screens-->
+            <!--        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">-->
+            <!--            <span class="navbar-toggler-icon"></span>-->
+            <!--        </button>-->
+
+            <!--        <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="login_page.php">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="registration_page.php">Register</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!--    Add breaks so the header won't overlap the main content-->
+    <br><br><br><br>
+    <!-- TODO @Ali replace breaks with proper margin -->
+
+</header>
--- login_page.php
+++ login_page.php
@@ -1,57 +1,5 @@
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+<?php require_once('header.php'); ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-
-    <!--    local CSS stylesheet-->
-    <link rel="stylesheet" href="css/style.css">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-
-    <title>Login Page</title>
-
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
-</head>
-<body>
-<header>
-    <nav class="navbar  navbar-expand-sm navbar-light navbar-laravel fixed-top bg-light text-dark ">
-        <div class="container">
-            <a class="navbar-brand" href="#">Ali and Daniel's Recipe Book</a>
-
-            <!--Code for hamburger menu that auto hides the navigation bar items on small screens-->
-            <!--        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">-->
-            <!--            <span class="navbar-toggler-icon"></span>-->
-            <!--        </button>-->
-
-            <!--        <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="/login_page.php">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/registration_page.php">Register</a>
-                </li>
-            </ul>
-            <!--        </div>-->
-        </div>
-    </nav>
-    <!--    Breaks so the header won't overlap the main content-->
-    <br><br><br><br>
-</header>
 <main class="login-form">
     <div class="container">
         <div class="row justify-content-center">
