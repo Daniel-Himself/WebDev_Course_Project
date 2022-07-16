c03d215f9bfbedb63b3c4731586aab70a5f3505b
Daniel <danielsharon97@gmail.com>
2022-07-16 17:21:50 +0300

    Streamlined password reset page

--- forgot.php
+++ forgot.php
@@ -4,20 +4,14 @@
 
 <?php function create_restore_password_link($email): string
 {
-    // echo "http://localhost/folderFile/restore.php?secret=$email";
-    // return "http://localhost/tirugl_12_forget_password/restore.php?secret=$email";
-
     return "./restore.php?secret=$email";
-} ?>
+}
 
-<?php
 if (isset($_POST['forgot_password'])) {
     $email = $_POST['email'];
-    // $user_password = $_POST['user_password'];
     $sql = "SELECT * FROM `users` WHERE email='$email'";
     $results =  $mysqli->query($sql);
     if ($results->num_rows > 0) {
-        // echo "user found";
         echo "<p>מייל לשחזור סיסמה</p>";
         $restore_url = create_restore_password_link($email);
         echo "<a href='$restore_url'>לחץ כאן לאיפוס סיסמה</a>";
@@ -28,32 +22,32 @@ if (isset($_POST['forgot_password'])) {
 }
 
 if (isset($_POST['forgotPASS']) || isset($_GET['Message1'])) {
-?>
-<main class="login-form" id="mainElement">
-    <div class="container" id="content-wrap">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Reset Password</div>
-                    <div class="card-body">
-                        <form method="POST">
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
-                                <div class="col-md-6">
-                                    <input type="email" id="login_email" class="form-control" name="email" required autofocus>
+    ?>
+    <main class="login-form" id="mainElement">
+        <div class="container" id="content-wrap">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">Reset Password</div>
+                        <div class="card-body">
+                            <form method="POST">
+                                <div class="form-group row">
+                                    <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
+                                    <div class="col-md-6">
+                                        <input type="email" id="login_email" class="form-control" name="email" required autofocus>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-md-6 offset-md-4">
-                                <button name='forgot_password' type="submit" class="btn btn-primary">
-                                    Send Request
-                                </button>
-                            </div>
-                        </form>
+                                <div class="col-md-6 offset-md-4">
+                                    <button name='forgot_password' type="submit" class="btn btn-primary">
+                                        Send Request
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</main>
+    </main>
 <?php }
 require_once('footer.php'); ?>
--- restore.php
+++ restore.php
@@ -7,11 +7,10 @@ $email = $_GET["secret"];
 $sql = "SELECT * FROM `users` WHERE email='$email'";
 $results =  $mysqli->query($sql);
 if ($results->num_rows < 0) {
-    die("קוד משתמש לא נכון");
+    die("No user found for given email address");
 }
 
 if(isset($_POST['restore_password'])) {
-    $email = $_POST['email'];
     $newpassword = $_POST['password'];
     $sql = "UPDATE `users` SET `password`='$newpassword' WHERE email = '$email'";
     $results =  $mysqli->query($sql);
@@ -23,11 +22,8 @@ if(isset($_POST['restore_password'])) {
     }
 }
 ?>
-<form method="post">
-    <br><br><br><br><br><br>
-    <label for="email">Email Address</label>
-    <input id="email" name="email" type="email"/>
-    <label for="password">New Password</label>
+<form id=mainElement method="post">
+    <label for="password">New password for <?php echo $_GET["secret"];?></label>
     <input id="password" name="password" type="password"/>
     <input type="submit" name="restore_password" value="Reset Password"/>
 </form>
